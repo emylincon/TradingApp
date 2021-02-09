8e68aa33ae41c8e458665fffd766bb97ec2a902a
Emeka Ugwuanyi <emylincon@gmail.com>
2021-02-09 17:24:23 +0000

    new trade prediction class

--- Trade.py
+++ Trade.py
@@ -3,99 +3,141 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
+from History import TradeHistory
+import time
 
 
 class Trade:
+    def __init__(self, name):
+        self.name = name
+        self.stock = TradeHistory(name=self.name)
+        self.data = None
+        self.classifier = None
+        self.keep_columns = ['Close', 'MACD', 'signal_Line', 'RSI', 'SMA', 'EMA']
+        self.accuracy = {'train': 0, 'test': 0}
+
+    def get_data(self, length='4hr'):
+        return self.stock.get_data(length=length, increment='1m')
+
+    def set_data(self, length='4hr'):
+        self.data = self.get_data(length)
+
+    def SMA(self, data, period=30, column='Close'):
+        # Create the Simple Moving Average (SMA)
+        return data[column].rolling(window=period).mean()
+
+    # Create the Exponential Moving Average (EMA)
+    def EMA(self, data, period=20, column='Close'):
+        return data[column].ewm(span=period, adjust=False).mean()
+
+    # Create a function to calculate the Moving Average Convergence / Divergence (MACD)
+    def MACD(self, data, period_long=26, period_short=12, period_signal=9, column='Close'):
+        # Calculate the Short Term EMA
+        ShortEMA = self.EMA(data, period=period_short, column=column)
+        # Calculate the Long Term EMA
+        LongEMA = self.EMA(data, period=period_long, column=column)
+        # Calculate and store the MACD into the data frame
+        data['MACD'] = ShortEMA - LongEMA
+
+        # Calculate the signal line and store it in a data frame
+        data['signal_Line'] = self.EMA(data, period=period_signal, column='MACD')
+
+        return data
+
+    # Create a Function to calculate the Relative Strength Index (RSI)
+    def RSI(self, data, period=14, column='Close'):
+        delta = data[column].diff(1)
+        delta = delta.dropna()
+        up = delta.copy()
+        down = delta.copy()
+        up[up < 0] = 0
+        down[down > 0] = 0
+        data['up'] = up
+        data['down'] = down
+        AVG_Gain = self.SMA(data, period, column='up')
+        AVG_Loss = abs(self.SMA(data, period, column='down'))
+        RS = AVG_Gain / AVG_Loss
+        RSI = 100.0 - (100.0 / (1.0 + RS))
+
+        data['RSI'] = RSI
+        return data
+
+    def data_preparation(self):
+        # Add the indicators to the data set
+        self.set_data()
+        self.MACD(self.data)
+        self.RSI(self.data)
+        self.data['SMA'] = self.SMA(self.data)
+        self.data['EMA'] = self.EMA(self.data)
+
+        # Create the Target column
+        self.data['Target'] = np.where(self.data['Close'].shift(-1) > self.data['Close'], 1, 0)
+
+        self.data = self.data.dropna()
+
+        # Split the data set into a feature or independent data set (X) and a Target or dependent data set (Y)
+        X = self.data[self.keep_columns].values[:-1]
+        Y = self.data['Target'].values[:-1]
+
+        return X, Y
+
+    def get_model(self):
+        X, Y = self.data_preparation()
+        # Split the data again but this time into 80% training and 20% testing data sets
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+        if not self.classifier:
+            # Create and train the decision tree classifier model
+            self.classifier = DecisionTreeClassifier().fit(X_train, Y_train)
+
+            # Check how well the model did on the training data set
+            self.accuracy['train'] = self.classifier.score(X_train, Y_train)
+
+            # Check how well the model did on the testing data set
+            self.accuracy['test'] = self.classifier.score(X_test, Y_test)
+        else:
+            # Create and train the decision tree classifier model
+            tree = DecisionTreeClassifier().fit(X_train, Y_train)
+
+            # Check how well the model did on the training data set
+            train = tree.score(X_train, Y_train)
+
+            # Check how well the model did on the testing data set
+            test = tree.score(X_test, Y_test)
+
+            if test > self.accuracy['test']:
+                self.classifier, self.accuracy['train'], self.accuracy['test'] = tree, train, test
+
+    def predict(self):
+        self.get_model()
+        last = self.data[self.keep_columns].values[-1]
+        return self.classifier.predict([last])[0]
+
+
+class Table:
     def __init__(self):
-        pass
-df = pd.read_csv('test.csv')
-# https://www.youtube.com/watch?v=UsPIDNmiSDM&ab_channel=ComputerScience
-#Createl functions to calculate the Simple Moving Average (SMA) & the Exponential Moving Average (EMA)
-#Typical time periods for moving averages are 15, 20, & 30
-#Create the Simple Moving Average (SMA)
+        self.last_close = None
+        self.last_prediction = None
+        self.right = 0
+        self.predictions = pd.DataFrame(None, columns=['Datetime', 'Actual', 'Predicted'])
 
-def SMA(data, period=30, column='Close'):
-    return data[column].rolling(window=period).mean()
+    def set_close_prediction(self, close, prediction):
+        self.last_close = close
+        self.last_prediction = prediction
 
-#Create the Exponential Moving Average (EMA)
-def EMA(data, period=20, column='Close'):
-    return data[column].ewm(span=period, adjust=False).mean()
+    def add_row(self, datetime, close):
+        if self.last_close:
+            actual = 1 if close > self.last_close else 0
+            self.predictions.append({'Datetime': datetime, 'Actual': actual, 'Prediction': self.last_prediction})
+            self.right += 1 if actual == self.last_prediction else self.right
 
-
-#Create a function to calculate the Moving Average Convergence / Divergence (MACD)
-def MACD(data, period_long=26, period_short=12, period_signal=9, column='Close'):
-    #Calculate the Short Term EMA
-    ShortEMA = EMA(data, period=period_short, column=column)
-    # Calculate the Long Term EMA
-    LongEMA = EMA(data, period=period_long, column=column)
-    # Calculate and store the MACD into the data frame
-    data['MACD'] = ShortEMA - LongEMA
-
-    # Calculate the signal line and store it in a data frame
-    data['signal_Line'] = EMA(data, period=period_signal, column='MACD')
-
-    return data
+    def score(self):
+        round((self.right/len(self.predictions))*100, 2)
 
 
-# Create a Function to calculate the Relative Strength Index (RSI)
-def RSI(data, period=14, column='Close'):
-    delta = data[column].diff(1)
-    delta = delta.dropna()
-    up = delta.copy()
-    down = delta.copy()
-    up[up < 0] = 0
-    down[down > 0] = 0
-    data['up'] = up
-    data['down'] = down
-    AVG_Gain = SMA(data, period, column='up')
-    AVG_Loss = abs(SMA(data, period, column='down'))
-    RS = AVG_Gain / AVG_Loss
-    RSI = 100.0 - (100.0 / (1.0 + RS))
-
-    data['RSI'] = RSI
-    return data
-
-
-#Add the indicators to the data set
-MACD(df)
-RSI(df)
-df['SMA'] = SMA(df)
-df['EMA'] = EMA(df)
-
-#Create the Target column
-df['Target'] = np.where(df['Close'].shift(-1) > df['Close'], 1, 0)
-# df = df[:29]
-df = df.dropna()
-
-#Split the data set into a feature or independent data set (X) and a Target or dependent data set (Y)
-keep_columns = ['Close', 'MACD', 'signal_Line', 'RSI', 'SMA', 'EMA']
-X = df[keep_columns].values
-Y = df['Target'].values
-
-#Split the data again but this time into 80% training and 20% testing data sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-
-#Create and train the decision tree classifier model
-tree = DecisionTreeClassifier().fit(X_train, Y_train)
-
-#Check how well the model did on the training data set
-print(tree.score(X_train, Y_train))
-
-
-#Check how well the model did on the testing data set
-print(tree.score(X_test, Y_test))
-
-
-#Show the model tree predicitons
-tree_predictions = tree.predict(X_test)
-print(tree_predictions)
-
-#Show the actual values from the test data
-print(Y_test)
-
-#Get the models metrics
-print(classification_report(Y_test, tree_predictions))
-
+t_name = 'EUR/USD'
+my_trade = Trade(name=t_name)
+print(my_trade.predict())
 
 
 
