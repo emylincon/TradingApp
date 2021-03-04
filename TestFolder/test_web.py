0dd8de2ac2d193d81140eaa75a613947da558506
emylincon <emylincon@gmail.com>
2021-03-04 12:00:33 +0000

    test web

---
+++ TestFolder/test_web.py
@@ -0,0 +1,25 @@
+import unittest
+from web import app, record
+
+
+class TestApp(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.tester = app.test_client(cls)
+
+    def test_index(self):
+        response = self.tester.get('/')
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+    def test_get_display(self):
+        response = record.get('EUR/USD')
+        response.trade.predict()
+        data = response.get_display_data()
+        self.assertListEqual(list(data.keys()),['table','close','SMA','EMA', 'accuracy', 'date','model'])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
