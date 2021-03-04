4cd48379af1ce0a339e2fd1503af2a356e566d34
emylincon <emylincon@gmail.com>
2021-03-04 18:02:44 +0000

    removed display

--- test_web.py
+++ test_web.py
@@ -13,11 +13,11 @@ class TestApp(unittest.TestCase):
         status_code = response.status_code
         self.assertEqual(status_code, 200)
 
-    def test_get_display(self):
-        response = record.get('EUR/USD')
-        response.trade.predict()
-        data = response.get_display_data()
-        self.assertListEqual(list(data.keys()),['table','close','SMA','EMA', 'accuracy', 'date','model'])
+    # def test_get_display(self):
+    #     response = record.get('EUR/USD')
+    #     response.trade.predict()
+    #     data = response.get_display_data()
+    #     self.assertListEqual(list(data.keys()),['table','close','SMA','EMA', 'accuracy', 'date','model'])
 
 
 if __name__ == '__main__':
