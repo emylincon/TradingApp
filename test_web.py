71a9be873d591baa52877e45bfbd4d79113d21e6
emylincon <emylincon@gmail.com>
2021-04-04 20:03:58 +0100

    update more tests

--- test_web.py
+++ test_web.py
@@ -24,12 +24,22 @@ class TestApp(unittest.TestCase):
         data = response.get_display_data()
         self.assertListEqual(list(data.keys()),['table','close','SMA','EMA', 'accuracy', 'date','model'])
 
-    # def test_get_display(self):
-    #     obj = record.get('EUR/USD')
-    #     obj.trade.predict()
-    #     response = self.tester.get('/display/EUR/USD')
-    #     self.assertListEqual(sorted(list(response.json.keys())),
-    #                          sorted(['table','close','SMA','EMA', 'accuracy', 'date','model']))
+    def test_dashboard(self):
+        response = self.tester.get('/dashboard')
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+    def test_display(self):
+        obj = record.get('EUR/USD')
+        obj.trade.predict()
+        response = self.tester.post('/display', json={'name': 'EUR/USD', 'ticker': 'EURUSD=X'})
+        self.assertListEqual(sorted(list(response.json.keys())),
+                             sorted(['table','close','SMA','EMA', 'accuracy', 'date','model']))
+
+    def test_post_dashboard(self):
+        f = {'myname': 'EUR/USD', 'myticker': 'EURUSD=X'}
+        response = self.tester.post('/dashboard', data=f)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
