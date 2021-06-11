d61cc6f93a095fc328e6ae1ca1e937ba4aab2458
emylincon <emylincon@gmail.com>
2021-06-11 22:10:10 +0100

    import refactor

--- Trade.py
+++ Trade.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report
 from History import TradeHistory
 import time
 import traceback, sys
